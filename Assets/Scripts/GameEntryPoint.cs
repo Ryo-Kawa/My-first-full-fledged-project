4c6dae7761f2513545948153ffcd6c4dd0497536
Kawaryo <152622724+Ryo-Kawa@users.noreply.github.com>
2025-02-18 09:48:49 +0900

    Update GameEntryPoint.cs

--- Assets/Scripts/GameEntryPoint.cs
+++ Assets/Scripts/GameEntryPoint.cs
@@ -10,8 +10,6 @@ public class GameEntryPoint : IAsyncStartable
     [Inject] private Func<HomeProcess> _createHomeProcess;
     [Inject] private Func<BattleProcess> _createBattleProcess;
 
-    private Level? m_level = null;
-
     public async UniTask StartAsync(CancellationToken cancellation)
     {
         SplashScreenProcess splashScreenProcess = _createSplashScreenProcess();
@@ -19,19 +17,5 @@ public class GameEntryPoint : IAsyncStartable
         HomeProcess homeProcess = _createHomeProcess();
     
         BattleProcess battleProcess = _createBattleProcess();
-    }
-
-    private void StartBattle()
-    {
-        //m_level = new Level(_game_objects, _camera, 10, 10);
-        //m_level.Init();
-    }
-
-    private void QuitBattle()
-    {
-        //m_level = null;
-
-        //_game_objects.Get("in_battle").SetActive(false);
-        //_game_objects.Get("out_battle").SetActive(true);
     }
 }
