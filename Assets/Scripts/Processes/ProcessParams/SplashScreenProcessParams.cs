797c803b6b4902b7e2393a0cc3aced8b88051dc5
Kawaryo <152622724+Ryo-Kawa@users.noreply.github.com>
2025-02-18 09:44:12 +0900

    Delete Assets/Scripts/Processes/ProcessParams/SplashScreenProcessParams.cs

--- Assets/Scripts/Processes/ProcessParams/SplashScreenProcessParams.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SplashScreenProcessParams", menuName = "SplashScreenProcessParams", order = 0)]
-public class SplashScreenProcessParams : ProcessParamsBase
-{
-    
-}
