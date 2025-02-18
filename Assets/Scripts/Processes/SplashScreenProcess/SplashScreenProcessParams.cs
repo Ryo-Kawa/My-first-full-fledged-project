4a695ab60ec6f3ce32043e58a249b1b4ed44605e
Kawaryo <152622724+Ryo-Kawa@users.noreply.github.com>
2025-02-18 09:44:30 +0900

    Create SplashScreenProcessParams.cs

---
+++ Assets/Scripts/Processes/SplashScreenProcess/SplashScreenProcessParams.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SplashScreenProcessParams", menuName = "SplashScreenProcessParams", order = 0)]
+public class SplashScreenProcessParams : ProcessParamsBase
+{
+    
+}
