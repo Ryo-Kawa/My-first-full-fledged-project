6ed4983115b73d247421aba7e8540622c623d755
Kawaryo <152622724+Ryo-Kawa@users.noreply.github.com>
2025-02-18 09:49:49 +0900

    Update BattleProcess.cs

--- Assets/Scripts/Processes/BattleProcess/BattleProcess.cs
+++ Assets/Scripts/Processes/BattleProcess/BattleProcess.cs
@@ -1,4 +1,12 @@
 public class BattleProcess : ProcessBase<BattleProcessParams>
 {
-    
+    private Level m_level = new();
+
+    private void QuitBattle()
+    {
+        //m_level = null;
+
+        //_game_objects.Get("in_battle").SetActive(false);
+        //_game_objects.Get("out_battle").SetActive(true);
+    }
 }
