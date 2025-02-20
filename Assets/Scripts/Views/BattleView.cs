06958390ed56dd1f0bce62771b5ea31e44fa3c89
Kawaryo <152622724+Ryo-Kawa@users.noreply.github.com>
2025-02-21 07:22:26 +0900

    Update BattleView.cs

--- Assets/Scripts/Views/BattleView.cs
+++ Assets/Scripts/Views/BattleView.cs
@@ -3,12 +3,12 @@ using UnityEngine.UI;
 
 public class GameView : ViewBase
 {
-    private readonly ObservableList<Button> _magicianCardButtons;
+    private readonly ObservableList<Button> _magicianCardButtons = new();
 
     public Button endTurnButton;
 
     private void Start()
     {
-        
+        _magicianCardButtons.ObserveAdd()
     }
 }
