42d4a410d334d0c4f1e6ffab4061e50152e4e524
Kawaryo <152622724+Ryo-Kawa@users.noreply.github.com>
2025-02-20 09:43:38 +0900

    Update Logic.cs

--- Assets/Scripts/Processes/BattleProcess/Logic.cs
+++ Assets/Scripts/Processes/BattleProcess/Logic.cs
@@ -1,9 +1,27 @@
 public class Logic
 {
   private Level _level;
+  private readonly GameObject _magicianCardPrefab;
   
   public Logic(Level level)
   {
-    this._level = level;
+      this._level = level;
+
+      Init();
+  }
+
+  private void Init()
+  {
+      for(int i = 0; i < 5; i++)
+      {
+          TakeOneCard();
+      }
+  }
+
+  private void TakeOneCard()
+  {
+      MagicianCard card = _level.deck.Pop();
+      GameObject cardObject = GameObject.Instantiate(_magicianCardPrefab);
+      _level.holdingCard.Add(card, );
   }
 }
